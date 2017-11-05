f544ed50a47226e760a691d94faedf104bd4c57c
Eric Sims <EricSimsM@gmail.com>
2017-11-04 21:40:19 -0400

    an actual server file

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const next = require('next');
+const express = require('express')
+const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
 const handle = app.getRequestHandler()
-//const api = express.Router();
 
-app.prepare().then(() => {
-  const server = express();
+app.prepare()
+.then(() => {
+  const server = express()
 
-   //app.use('/api', api);
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
 
-  server.listen(3000, (err) => {
+  server.listen(port, (err) => {
     if (err) throw err
-    console.log('Ready on http://localhost:3000')
-  });
-
-}).catch((ex) => {
-  console.error(ex.stack);
-  process.exit(1);
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
