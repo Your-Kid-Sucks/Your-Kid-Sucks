39114b39bae7249fa0a41a09ea75465f99d9eec3
Eric Sims <EricSimsM@gmail.com>
2017-11-04 20:48:56 -0400

    a working index page

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const next = require('next');
-const app = next();
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
 const handle = app.getRequestHandler()
-const api = express.Router();
+//const api = express.Router();
 
 app.prepare().then(() => {
   const server = express();
 
-  app.use('/api', api);
+   //app.use('/api', api);
 
   server.listen(3000, (err) => {
     if (err) throw err
     console.log('Ready on http://localhost:3000')
-  })
+  });
 
- }).catch((ex) => {
+}).catch((ex) => {
   console.error(ex.stack);
   process.exit(1);
 })
