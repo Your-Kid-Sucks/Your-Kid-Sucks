a65ac337851d80289cda0a0891e8617fa8cdac35
Eric Sims <EricSimsM@gmail.com>
2017-11-04 22:24:29 -0400

    started database stuff

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-const express = require('express')
-const next = require('next')
+const express = require('express');
+const next = require('next');
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.prepare()
-.then(() => {
-  const server = express()
+app.prepare().then(() => {
+  const server = express();
+  const api = express.Router();
+
+  api.get('/', (req, res) => {
+    res.send('Welcome to the API!!!');
+  });
+
+  server.use('/api', api);
 
   server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+    return handle(req, res);
+  });
 
   server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
