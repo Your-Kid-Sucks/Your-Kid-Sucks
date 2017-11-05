9293a6c968877b8b3b9bd599998a34229d39af1a
Eric Sims <EricSimsM@gmail.com>
2017-11-04 20:25:01 -0400

    an app that only throws 404 :)

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const next = require('next');
+const app = next();
+const handle = app.getRequestHandler()
+const api = express.Router();
+
+app.prepare().then(() => {
+  const server = express();
+
+  app.use('/api', api);
+
+  server.listen(3000, (err) => {
+    if (err) throw err
+    console.log('Ready on http://localhost:3000')
+  })
+
+ }).catch((ex) => {
+  console.error(ex.stack);
+  process.exit(1);
+})
